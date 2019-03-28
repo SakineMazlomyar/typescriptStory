69eaac5302b0248b40eb5bc7afcd343c13319936
SakineMazlomyar <mazlomyar12@gmail.com>
2019-03-28 19:03:45 +0100

    addedTypes

--- script.ts
+++ script.ts
@@ -1,12 +1,8 @@
-interface Choices {
-    left:{id:number, answer:string},
-    right:{id:number, answer:string}
-}
 class StoryStep {
     id:number;
     question:string;
-    cases:Choices;
-    constructor(id:number, question:string, cases:Choices){
+    cases: any;
+    constructor(id:number, question:string, cases:any){
         this.id = id
         this.question = question
         this.cases = cases;
@@ -14,11 +10,8 @@ class StoryStep {
 
     }
 }
-/* let storySteps: object[] = [object]; */
-let storySteps: Array<object> = [];
 
-/* let stepOneChoice = {left:{id:2, answer:"dog"}, right:{id:3, answer:"horse"}}
-getChoice(stepOneChoice) */
+let storySteps : {id:number, question:string, cases:{left:{id:number, answer:string}, right:{id:number, answer:string} }}[] = []
 let stepOne = new StoryStep(1, "Choose dog   Or horse",{left:{id:2, answer:"dog"}, right:{id:3, answer:"horse"}});
 let stepTwo = new StoryStep(2, "Choose  cat or mouse", {left:{id:4, answer:"cat"}, right:{id:5, answer:"mouse"}});
 let stepThree = new StoryStep(3, "Choose between get and sushi", {left:{id:4, answer:"get"}, right:{id:5, answer:"sushi"}});
@@ -26,7 +19,7 @@ let stepFour = new StoryStep(4, "choose tree or pasta", {left:{id:1, answer:"tre
 let stepFive = new StoryStep(5, "Choose typescript or javascript", {left:{id:1, answer:"typescript"}, right:{id:2, answer:"vanila/javascript"}});
 storySteps.push(stepOne, stepTwo, stepThree, stepFour, stepFive)
 
-let curreStep: object
+let curreStep: {id:number, question:string, cases:{left:{id:number, answer:string}, right:{id:number, answer:string} }}
 let leftButton: HTMLElement | null
 let rightButton: HTMLElement | null
 let pTagg: HTMLElement | null
